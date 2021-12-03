615eaf6ab12af9125ed066d725cf15c30a72ee2f
JuviaBerry <47364179+JuviaBerry@users.noreply.github.com>
2021-12-03 10:19:32 +0100

    Link js to redirection

---
+++ script.js
@@ -0,0 +1,14 @@
+//JAVASCRIPT FILE 
+
+//TEST
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Test.";
+}
+
+function search(){
+  location.href = "./results_cheese.html" ;
+}
+
+function detail(){
+  location.href = "./detail.html" ;
+}
