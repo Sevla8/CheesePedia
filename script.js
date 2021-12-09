8bd1e4fb2132d2213a291429e0ba24a086b25590
Brandon Alves <brandon.alves.barbosa@gmail.com>
2021-12-09 18:29:18 +0100

    update filters

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 //JAVASCRIPT FILE
 
+// Appuyer sur ENTER pour rechercher
 document.addEventListener('keypress', (event) => {
 	var name = event.key;
 	if (name == 'Enter') {
 		search();
 	}
-  }, false);
+}, false);
 
-function search(){
+// Construit la l'URL de la page
+function search() {
 	let searchTxt = document.getElementById("searchTxt").value;
 	searchTxt = encodeURIComponent(searchTxt);
+
+	let certificationTxt = document.getElementById("certificationFilters").value;
+	certificationTxt = encodeURIComponent(certificationTxt);
 
 	let countryTxt = document.getElementById("countryFilters").value;
 	countryTxt = encodeURIComponent(countryTxt);
 
+	let textureTxt = document.getElementById("textureFilters").value;
+	textureTxt = encodeURIComponent(textureTxt);
+
+	let sourceTxt = document.getElementById("sourceFilters").value;
+	sourceTxt = encodeURIComponent(sourceTxt);
+
 	let pasteurizedTxt = document.getElementById("pasteurizedFilters").value;
 	pasteurizedTxt = encodeURIComponent(pasteurizedTxt);
 
-	location.href = `./results_cheese.html?search=${searchTxt}&country=${countryTxt}&pasteurized=${pasteurizedTxt}`;
+
+	location.href = `./results_cheese.html?search=${searchTxt}&certification=${certificationTxt}&country=${countryTxt}&texture=${textureTxt}&source=${sourceTxt}&pasteurized=${pasteurizedTxt}`;
 }
 
-//Affichage de la liste des fromages (inspiré du code moodle)
+// Obtention de la liste des fromages
 function searchCheeses() {
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
@@ -28,89 +40,90 @@ function searchCheeses() {
 		var inputLabel = decodeURIComponent(urlParams.get('search'));
 		console.log('Search:', inputLabel);
 	}
+	if (urlParams.has('certification')) {
+		var inputCertification = decodeURIComponent(urlParams.get('certification'));
+		console.log('Certification:', inputCertification);
+	}
 	if (urlParams.has('country')) {
 		var inputCountry = decodeURIComponent(urlParams.get('country'));
 		console.log('Country:', inputCountry);
+	}
+	if (urlParams.has('texture')) {
+		var inputTexture = decodeURIComponent(urlParams.get('texture'));
+		console.log('Texture:', inputTexture);
+	}
+	if (urlParams.has('source')) {
+		var inputSource = decodeURIComponent(urlParams.get('source'));
+		console.log('Source:', inputSource);
 	}
 	if (urlParams.has('pasteurized')) {
 		var inputPasteurized = decodeURIComponent(urlParams.get('pasteurized'));
 		console.log('Pasteurized:', inputPasteurized);
 	}
 
-	var contenu_requete = "";
-	if (inputCountry == "" && inputPasteurized == "") {
-		contenu_requete = `
-			SELECT ?label ?thumbnail
-			WHERE {
-				?cheese a dbo:Cheese ;
-						dbo:abstract ?abstract ;
-						rdfs:label ?label .
-				FILTER(
-					langMatches(lang(?label),"EN") &&
-					langMatches(lang(?abstract),"EN") &&
-					REGEX(?abstract ,"cheese", "i") &&
-					REGEX(?label, "${inputLabel}", "i")
-				)
-				OPTIONAL {
-					?cheese dbo:thumbnail ?thumbnail .
-				}
-			}
-			ORDER BY ASC(?label)
-		`;
-	}
-	else if (inputCountry == "" && inputPasteurized != "") {
-		contenu_requete = `
-		SELECT ?label ?thumbnail ?pasteurized
+	var contenu_requete = `
+		SELECT ?label ?thumbnail
 		WHERE {
 			?cheese a dbo:Cheese ;
 					dbo:abstract ?abstract ;
-					dbp:pasteurized ?pasteurized ;
 					rdfs:label ?label .
 			FILTER(
-				langMatches(lang(?label),"EN") &&
-				langMatches(lang(?abstract),"EN") &&
-				langMatches(lang(?pasteurized), "EN") &&
+				langMatches(lang(?label), "EN") &&
+				langMatches(lang(?abstract), "EN") &&
 				REGEX(?abstract ,"cheese", "i") &&
-				REGEX(?pasteurized, "${inputPasteurized}", "i") &&
 				REGEX(?label, "${inputLabel}", "i")
 			)
+	`;
+	if (inputCertification != "") {
+		contenu_requete += `
+			?cheese dbp:certification ?certification .
+			FILTER(
+				REGEX(?certification, "${inputCertification}", "i")
+			)
+		`;
+	}
+	if (inputCountry != "") {
+		contenu_requete += `
+			?cheese dbp:country ?country .
+			FILTER(
+				REGEX(?country, "${inputCountry}", "i")
+			)
+		`;
+	}
+	if (inputTexture != "") {
+		contenu_requete += `
+			?cheese dbp:texture ?texture .
+			FILTER(
+				REGEX(?texture, "${inputTexture}", "i")
+			)
+		`;
+	}
+	if (inputSource != "") {
+		contenu_requete += `
+			?cheese dbp:source ?source .
+			FILTER(
+				REGEX(?source, "${inputSource}", "i")
+			)
+		`;
+	}
+	if (inputPasteurized != "") {
+		contenu_requete += `
+			?cheese dbp:pasteurized ?pasteurized .
+			FILTER(
+				REGEX(?pasteurized, "${inputPasteurized}", "i")
+			)
+		`;
+	}
+	contenu_requete += `
 			OPTIONAL {
 				?cheese dbo:thumbnail ?thumbnail .
 			}
 		}
+		GROUP BY ?label ?thumbnail
 		ORDER BY ASC(?label)
-		`;
-	}
-	else if (inputCountry != "" && inputPasteurized == "") {
-		contenu_requete = `
-			SELECT ?label ?thumbnail ?country
-			WHERE {
-				?cheese a dbo:Cheese ;
-						dbo:abstract ?abstract ;
-						dbp:country ?country0 ;
-						rdfs:label ?label .
-				FILTER(
-					langMatches(lang(?label),"EN") &&
-					langMatches(lang(?abstract),"EN") &&
-					REGEX(?abstract ,"cheese", "i") &&
-					REGEX(?label, "${inputLabel}", "i") &&
-					REGEX(?country0, "${inputCountry}", "i")
-				)
-				OPTIONAL {
-					?cheese dbo:thumbnail ?thumbnail .
-				}
-				OPTIONAL {
-					?country0 rdfs:label ?country_label .
-					FILTER(
-						langMatches(lang(?country_label), "EN") &&
-						REGEX(?country_label, "${inputCountry}", "i")
-					)
-				}
-				BIND(COALESCE(?country_label, ?country0) AS ?country)
-			}
-			ORDER BY ASC(?label)
-		`;
-	}
+	`;
+
+	console.log('Request:', contenu_requete);
 
     // Encodage de l'URL à transmettre à DBPedia
     var url_base = "http://dbpedia.org/sparql";
@@ -128,8 +141,8 @@ function searchCheeses() {
     xmlhttp.send();
 }
 
-// Affichage des résultats dans un tableau (inspiré du code moodle)
-function afficherResultats(data){
+// Affichage des résultats dans un tableau
+function afficherResultats(data) {
 	console.log('Results from https://dbpedia.org/:', data);
 
 	let i = 0;
@@ -167,8 +180,6 @@ function afficherResultats(data){
 			result += '<p class="thumbnail"><img class="img-result" src="ressources/defaultImg.png"\" target="_blank"></p>';
 		}
 
-
-
 		result += '<p><a href=detail.html?cheese=' + encodeURIComponent(cheese.label.value) + '>More details</a></p>';
 
 		result += '</td>';
@@ -176,8 +187,8 @@ function afficherResultats(data){
 		++i;
 
 		if (i == 4) {
-		result += "</r><tr class='table-row'>";
-		i = 0;
+			result += "</r><tr class='table-row'>";
+			i = 0;
 		}
 	});
 	result += "</tr>";
@@ -186,9 +197,8 @@ function afficherResultats(data){
 
 }
 
-function loadDetail(){
-
-  const queryString = window.location.search;
+function loadDetail() {
+  	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
 	if (urlParams.has('cheese')) {
 		var inputLabel = decodeURIComponent(urlParams.get('cheese'));
@@ -197,37 +207,38 @@ function loadDetail(){
 		console.log('Detail of cheese:', inputLabel);
 	}
 
-	var contenu_requete = `select ?f ?a ?n ?thumbnail  sum(if(regex(?c,"A.*O.*C"),1,0)) as ?AOC 
-              sum(if(regex(?c,"AOP"),1,0)) as ?AOP
-              sum(if(regex(?c,"P.*D.*O"),1,0)) as ?PDO 
-              sum(if(regex(?c,"D.*O.*C"),1,0)) as ?DOC 
-              sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ff]irm"),1,0)) as ?SFirm 
-              sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ha]ard"),1,0)) as ?SHard
-             sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ss]oft"),1,0)) as ?SSoft
-              sum(if(regex(?t,"[Ff]irm"),1,0))-sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ff]irm"),1,0)) as ?Firm
-              sum(if(regex(?t,"[Ss]oft"),1,0))-sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ss]oft"),1,0)) as ?Soft
-              sum(if(regex(?t,"[Hh]ard"),1,0))-sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Hh]ard"),1,0)) as ?Hard
-             sum(if(regex(?t,"[Cc]rumbly"),1,0)) as ?Crumbly
-             sum(if(regex(?t,"[Cc]ream"),1,0)) as ?Creamy
-             sum(if(regex(?t,"[Dd]ense"),1,0)) as ?Dense
-             sum(if(regex(?t,"[Cc]ompact"),1,0)) as ?Compact
-             sum(if(regex(?t,"[Gg]ranular"),1,0)) as ?Granular
-             sum(if(regex(?t,"[Mm]oist"),1,0)) as ?Moist
-             sum(if(regex(?t,"[Ee]lastic"),1,0)) as ?Elastic
-             sum(if(regex(?t,"[Ss]tringy"),1,0)) as ?Stringy
-             sum(if(regex(?t,"[Ss]mooth"),1,0)) as ?Smooth
-             sum(if(regex(?s,"[Gg]oat"),1,0)) as ?Goat
-             sum(if(regex(?s,"[Cc]ow|[Cc]attle"),1,0)) as ?Cow
-             sum(if(regex(?s,"[Ss]heep|[Ee]we"),1,0)) as ?Sheep
-             sum(if(regex(?s,"[Bb]uffalo|[Cc]arabao"),1,0)) as ?Buffalo
-             sum(if(regex(?s,"[Dd]onkey"),1,0)) as ?Donkeys
-             sum(if(regex(?s,"[Yy]ak"),1,0)) as ?Yak
-             sum(if(regex(?s,"[Mm]oose"),1,0)) as ?Moose
-             sum(if(regex(?p,"([Yy]es|[Oo]ften|[Ff]requently|[Pp]ossibly|[Dd]epends)"),1,0))+sum(if(regex(?p2,"([Yy]es|[Oo]ften|[Ff]requently|[Pp]ossibly|[Dd]epends)"),1,0)) as ?Pasteurized
-
-						 where {
+	var contenu_requete = `
+		select ?f ?a ?n ?thumbnail
+		sum(if(regex(?c,"A.*O.*C"),1,0)) AS ?AOC
+		sum(if(regex(?c,"AOP"),1,0)) as ?AOP
+		sum(if(regex(?c,"P.*D.*O"),1,0)) as ?PDO
+		sum(if(regex(?c,"D.*O.*C"),1,0)) as ?DOC
+		sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ff]irm"),1,0)) as ?SFirm
+		sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ha]ard"),1,0)) as ?SHard
+		sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ss]oft"),1,0)) as ?SSoft
+		sum(if(regex(?t,"[Ff]irm"),1,0))-sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ff]irm"),1,0)) as ?Firm
+		sum(if(regex(?t,"[Ss]oft"),1,0))-sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Ss]oft"),1,0)) as ?Soft
+		sum(if(regex(?t,"[Hh]ard"),1,0))-sum(if(regex(?t,"([Ss]emi|[Mm]edium).?[Hh]ard"),1,0)) as ?Hard
+		sum(if(regex(?t,"[Cc]rumbly"),1,0)) as ?Crumbly
+		sum(if(regex(?t,"[Cc]ream"),1,0)) as ?Creamy
+		sum(if(regex(?t,"[Dd]ense"),1,0)) as ?Dense
+		sum(if(regex(?t,"[Cc]ompact"),1,0)) as ?Compact
+		sum(if(regex(?t,"[Gg]ranular"),1,0)) as ?Granular
+		sum(if(regex(?t,"[Mm]oist"),1,0)) as ?Moist
+		sum(if(regex(?t,"[Ee]lastic"),1,0)) as ?Elastic
+		sum(if(regex(?t,"[Ss]tringy"),1,0)) as ?Stringy
+		sum(if(regex(?t,"[Ss]mooth"),1,0)) as ?Smooth
+		sum(if(regex(?s,"[Gg]oat"),1,0)) as ?Goat
+		sum(if(regex(?s,"[Cc]ow|[Cc]attle"),1,0)) as ?Cow
+		sum(if(regex(?s,"[Ss]heep|[Ee]we"),1,0)) as ?Sheep
+		sum(if(regex(?s,"[Bb]uffalo|[Cc]arabao"),1,0)) as ?Buffalo
+		sum(if(regex(?s,"[Dd]onkey"),1,0)) as ?Donkeys
+		sum(if(regex(?s,"[Yy]ak"),1,0)) as ?Yak
+		sum(if(regex(?s,"[Mm]oose"),1,0)) as ?Moose
+		sum(if(regex(?p,"([Yy]es|[Oo]ften|[Ff]requently|[Pp]ossibly|[Dd]epends)"),1,0))+sum(if(regex(?p2,"([Yy]es|[Oo]ften|[Ff]requently|[Pp]ossibly|[Dd]epends)"),1,0)) as ?Pasteurized
+		WHERE {
 			?f a dbo:Cheese.
-			?f dbo:abstract ?a. 
+			?f dbo:abstract ?a.
 			?f rdfs:label ?n.
 			OPTIONAL{?f dbp:certification ?c.}
 			OPTIONAL{?f dbp:pasteurised ?p}.
@@ -236,11 +247,15 @@ function loadDetail(){
 			OPTIONAL{?f dbp:texture ?t}.
 			OPTIONAL {?f dbo:thumbnail ?thumbnail}.
 
-
-			FILTER(langMatches(lang(?n),"EN") && langMatches(lang(?a),"EN") && REGEX(?a ,"[Cc]heese") && ?n=${inputLabel}).
-			}
-			GROUP BY ?f ?a ?n ?thumbnail
-  `;
+			FILTER(
+				langMatches(lang(?n),"EN") &&
+				langMatches(lang(?a),"EN") &&
+				REGEX(?a ,"[Cc]heese") &&
+				?n=${inputLabel}
+			).
+		}
+		GROUP BY ?f ?a ?n ?thumbnail
+	`;
 
 	// Encodage de l'URL à transmettre à DBPedia
     var url_base = "http://dbpedia.org/sparql";
@@ -256,123 +271,124 @@ function loadDetail(){
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-
-
 }
 
 function capitalizeFirstLetter(string) {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function afficherDetails(data){
+function afficherDetails(data) {
 	console.log('Detail from https://dbpedia.org/:', data);
 
 	data.results.bindings.forEach((cheese) => {
 
-		var certification="";
-		if (cheese.AOC.value==1) {
+		var certification = "";
+		if (cheese.AOC.value == 1) {
 			certification += "AOC, ";
 		}
-		if (cheese.AOP.value==1) {
+		if (cheese.AOP.value == 1) {
 			certification += "AOP, ";
 		}
-		if (cheese.PDO.value==1) {
+		if (cheese.PDO.value == 1) {
 			certification += "PDO, ";
 		}
-		if (cheese.DOC.value==1) {
+		if (cheese.DOC.value == 1) {
 			certification += "DOC, ";
 		}
-		if (certification==""){
+		if (certification == "") {
 			certification="-";
-		}else{
+		}
+		else {
 			certification = certification.substring(0, certification.length - 2);
 		}
 
-		var milk="";
-		if(cheese.Goat.value==1){
+		var milk = "";
+		if (cheese.Goat.value == 1) {
 			milk += "Goat, ";
 		}
-		if(cheese.Cow.value==1){
+		if (cheese.Cow.value == 1) {
 			milk += "Cow, ";
 		}
-		if(cheese.Sheep.value==1){
+		if (cheese.Sheep.value == 1) {
 			milk += "Sheep, ";
 		}
-		if(cheese.Buffalo.value==1){
+		if (cheese.Buffalo.value == 1) {
 			milk += "Buffalo, ";
 		}
-		if(cheese.Donkeys.value==1){
+		if (cheese.Donkeys.value == 1) {
 			milk += "Donkeys, ";
 		}
-		if(cheese.Yak.value==1){
+		if (cheese.Yak.value == 1) {
 			milk += "Yak, ";
 		}
-		if(cheese.Moose.value==1){
+		if (cheese.Moose.value == 1) {
 			milk += "Moose, ";
 		}
-		if (milk==""){
+		if (milk == "") {
 			milk="-";
-		}else{
+		}
+		else {
 			milk = milk.substring(0, milk.length - 2);
 		}
 
-		var pasteurized=""
-		if(cheese.Pasteurized.value==1){
+		var pasteurized = ""
+		if (cheese.Pasteurized.value == 1) {
 			pasteurized = "Yes";
-		}else{
+		}
+		else {
 			pasteurized = "No";
 		}
 
-
-		var texture="";
-		if(cheese.SFirm.value==1){
+		var texture = "";
+		if(cheese.SFirm.value == 1) {
 			texture += "SFirm, ";
 		}
-		if(cheese.SHard.value==1){
+		if (cheese.SHard.value == 1) {
 			texture += "SHard, ";
 		}
-		if(cheese.SSoft.value==1){
+		if (cheese.SSoft.value == 1){
 			texture += "SSoft, ";
 		}
-		if(cheese.Firm.value==1){
+		if (cheese.Firm.value == 1) {
 			texture += "Firm, ";
 		}
-		if(cheese.Soft.value==1){
+		if (cheese.Soft.value == 1){
 			texture += "Soft, ";
 		}
-		if(cheese.Hard.value==1){
+		if (cheese.Hard.value == 1) {
 			texture += "Hard, ";
 		}
-		if(cheese.Crumbly.value==1){
+		if (cheese.Crumbly.value == 1) {
 			texture += "Crumbly, ";
 		}
-		if(cheese.Creamy.value==1){
+		if (cheese.Creamy.value == 1) {
 			texture += "Creamy, ";
 		}
-		if(cheese.Dense.value==1){
+		if (cheese.Dense.value == 1) {
 			texture += "Dense, ";
 		}
-		if(cheese.Compact.value==1){
+		if (cheese.Compact.value == 1) {
 			texture += "Compact, ";
 		}
-		if(cheese.Granular.value==1){
+		if (cheese.Granular.value == 1) {
 			texture += "Granular, ";
 		}
-		if(cheese.Moist.value==1){
+		if (cheese.Moist.value == 1) {
 			texture += "Moist, ";
 		}
-		if(cheese.Elastic.value==1){
+		if (cheese.Elastic.value == 1) {
 			texture += "Elastic, ";
 		}
-		if(cheese.Stringy.value==1){
+		if (cheese.Stringy.value == 1) {
 			texture += "Stringy, ";
 		}
-		if(cheese.Smooth.value==1){
+		if (cheese.Smooth.value == 1) {
 			texture += "Smooth, ";
 		}
-		if (texture==""){
+		if (texture=="") {
 			texture="-";
-		}else{
+		}
+		else {
 			texture = texture.substring(0, texture.length - 2);
 		}
 
@@ -382,13 +398,8 @@ function afficherDetails(data){
 		document.getElementById("texture").innerHTML = texture;
 		document.getElementById("name").innerHTML = cheese.n.value;
 		document.getElementById("detail-block-left").innerHTML = cheese.a.value;
-		if(cheese.thumbnail){
+		if (cheese.thumbnail) {
 			document.getElementById("img-detail").src =  cheese.thumbnail.value;
 		}
-		
-
-
 	});
-
-
 }
